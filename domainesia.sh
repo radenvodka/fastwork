776864b6c2477942a49e517415f7f657b70fc408
ʀᴀᴅᴇɴᴠᴏᴅᴋᴀ (ᴇᴋᴀ ꜱ) <mr.ekasyahwan@gmail.com>
2022-05-30 10:43:28 -0700

    Update domainesia.sh

--- domainesia.sh
+++ domainesia.sh
@@ -17,6 +17,14 @@ systemctl start firewalld
 firewall-cmd --permanent --zone=public --add-port=2323/tcp
 firewall-cmd --reload
 
+# PHP 7.4
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install yum-utils
+yum-config-manager --enable remi-php74
+yum update
+yum install php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
+
 # Install composer
 wget https://getcomposer.org/installer -O composer-installer.php
 php composer-installer.php --filename=composer --install-dir=/usr/bin
