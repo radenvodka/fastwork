fd4674ff9c857d117fb4d1dbaeb9fa8515d93ce1
ʀᴀᴅᴇɴᴠᴏᴅᴋᴀ (ᴇᴋᴀ ꜱ) <mr.ekasyahwan@gmail.com>
2022-05-30 10:30:52 -0700

    Update domainesia.sh

--- domainesia.sh
+++ domainesia.sh
@@ -2,16 +2,22 @@
 # 1 GB RAM - 1 CPU Core
 # 20 GB Storage
 # PHP 7.4 & CHANGE PORT 22 TO 2323
+
 yum install nano firewalld
+
+# SSH CONFIG OPEN PORT 2323
 nano /etc/ssh/sshd_config
+semanage port -a -t ssh_port_t -p tcp 2323
 systemctl restart sshd.service
+
+# FIREWALL PORT
+
 systemctl enable firewalld
 systemctl start firewalld
 firewall-cmd --permanent --zone=public --add-port=2323/tcp
 firewall-cmd --reload
+
 # Install composer
-yum install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
-yum install wget -y
 wget https://getcomposer.org/installer -O composer-installer.php
 php composer-installer.php --filename=composer --install-dir=/usr/bin
 composer -V
